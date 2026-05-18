c6d2e6698a59e4cc6a8e226d56e9bce8a66aa561
Raíssa Bonfim <raissa230306@edu.unipar.br>
2026-05-18 20:07:10 -0300

    RF-007 destaque do dia atual

--- components/DiaCard.jsx
+++ components/DiaCard.jsx
@@ -1,44 +1,58 @@
-import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+
 import GameCard from './GameCard'
+
 import { formatarData } from '../utils/DateFormat'
 
 export default function DiaCard({ data, jogos }) {
-    
-    const hoje = formatarData(new Date())
-    const isDataAtual = data === hoje
 
-    console.log('klasdjlçasjd', data, hoje, isDataAtual)
+  const hoje = formatarData(new Date().toISOString().split('T')[0])
 
-    return (
-        <View style={[styles.card, isDataAtual && styles.jogoDeHoje]}>
-            <Text style={styles.data}>
-                {data}
-            </Text>
-            {
-                jogos.map((jogo) => (
-                    <GameCard key={jogo.id} game={jogo} />
-                ))
-            }
-        </View>
-    )
+  const isHoje = data === hoje
+
+  return (
+
+    <View style={[styles.card, isHoje && styles.cardHoje]}>
+
+      <Text style={[styles.data, isHoje && styles.dataHoje]}>
+        {data}
+      </Text>
+
+      {jogos.map((game) => (
+        <GameCard
+          key={game.id}
+          game={game}
+        />
+      ))}
+
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-    card: {
-        marginTop: 20,
-        backgroundColor: '#0c1b2a',
-        width: 320,
-        borderRadius: 12,
-        padding: 15,
-    },
-    data: {
-        color: '#f2cc2f',
-        fontSize: 22,
-        fontWeight: 'bold',
-        marginBottom: 10
-    },
-    jogoDeHoje: {
-        backgroundColor: '#000000',
-    }
+
+  card: {
+    marginTop: 20,
+    backgroundColor: '#0c1b2a',
+    width: 320,
+    borderRadius: 12,
+    padding: 15,
+  },
+
+  cardHoje: {
+    borderWidth: 2,
+    borderColor: '#f2cc2f'
+  },
+
+  data: {
+    color: '#f2cc2f',
+    fontSize: 22,
+    fontWeight: 'bold',
+    marginBottom: 10
+  },
+
+  dataHoje: {
+    color: '#ffffff'
+  }
+
 })
